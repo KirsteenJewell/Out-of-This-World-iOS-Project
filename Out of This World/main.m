4b60345356472694c7c18dd519e039966af30443
Kirsteen Jewell <kirsteen.jewell@gmail.com>
2014-05-01 12:10:01 +0100

    Initial Commit

---
+++ Out of This World/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Out of This World
+//
+//  Created by Kirsteen Jewell on 01/05/2014.
+//  Copyright (c) 2014 limitless. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KJAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KJAppDelegate class]));
+    }
+}
